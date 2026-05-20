7d45b4736917682a8061b4cd4a3478606f9718dd
mamminvest <martin@mamminvest.com>
2026-05-20 15:47:23 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MammInvestWebsite from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MammInvestWebsite />
+  </React.StrictMode>
+)
